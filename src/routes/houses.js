7a66d5286ff00d2f859195b6e42d8b89b9664f6d
RaquelJaldo <128419382+RaquelJaldo@users.noreply.github.com>
2023-06-30 21:11:06 +0200

    formulario añadir viviendas

--- src/routes/houses.js
+++ src/routes/houses.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { authRequired } = require('../middlewares/validateToken.js');
+
 
 houseController =require('../controllers/houseController.js');
 
 router.post('/', houseController.addHouse);
-router.get('/:houseId?', authRequired, houseController.getHouses); 
-router.put('/:houseId', authRequired,houseController.updateHouse);
-router.delete('/:houseId', houseController.deleteHouse);
+router.get('/:houseId?', authRequired, houseController.getHouse); 
+router.put('/:houseId', authRequired, houseController.updateHouse);
+router.delete('/:houseId',authRequired, houseController.deleteHouse);
 
 module.exports = router;
